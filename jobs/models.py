bd214eead51fa0d35f84d3e7034c745293327b3e
Denis <dineresc@gmail.com>
2021-04-24 14:51:57 -0300

    saving curr state

--- jobs/models.py
+++ jobs/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Position(models.Model):
     name = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
--- skills/models.py
+++ skills/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Skill(models.Model):
     title = models.CharField(null=False, blank=False, max_length=30)
     is_active = models.BooleanField(default=True)
