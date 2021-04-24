e3bc090e2399fa7e3034142a09eb10d7ad1866c5
Denis <dineresc@gmail.com>
2021-04-24 18:28:45 -0300

    gitignore

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include(('users.urls', 'user'), namespace='user')),
+    path('jobs/', include(('jobs.urls', 'jobs'), namespace='jobs'))
 ]
