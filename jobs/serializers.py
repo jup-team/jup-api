f03f5daa6a592d871b78a3aa1a7920f5ebe4dd2c
Denis <dineresc@gmail.com>
2021-04-25 15:39:32 -0300

    creating serializer for position

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -31,3 +31,9 @@ class JobDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = '__all__'
+
+
+class PositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Position
+        fields = ('id', 'name')
