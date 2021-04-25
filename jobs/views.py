e12993ea89ae4c7c9b5a304a280ad890a8235d52
Denis <dineresc@gmail.com>
2021-04-25 00:38:30 -0300

    adding check if user owner is the same of the user logged in

--- jobs/views.py
+++ jobs/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import Job
 from .serializers import JobCardSerializer, JobDetailSerializer
+from .utils import check_user_owner
 
 
 class JobsViewSet(viewsets.ViewSet):
@@ -11,9 +12,13 @@ class JobsViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = JobDetailSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        if check_user_owner(request):
+            serializer = JobDetailSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data={"message": "user token and owner of job does not match"},
+                            status=status.HTTP_400_BAD_REQUEST)
