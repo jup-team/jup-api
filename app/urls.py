c617959b28c1cfbea5e8833eabf072ecff8393b7
Denis <dineresc@gmail.com>
2021-05-02 01:05:19 -0300

    add url in app

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from jobs.views import PositionViewSet
+from skills.views import SkillsViewSet
 
 router = DefaultRouter()
 router.register(r'positions', PositionViewSet)
+router.register(r'skills', SkillsViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
